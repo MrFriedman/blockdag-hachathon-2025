3e440a2e0c8cf65024d98dc9b65b630d038deb0b
Dylan Friedman <117675543+MrFriedman@users.noreply.github.com>
2025-08-24 07:48:23 +0200

    MEOW.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @next/next/no-img-element
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
